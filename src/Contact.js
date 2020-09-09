72d58eb839434f0b839f747b95a147594ae693e2
James Lake <lovetobuildstuff@gmail.com>
2020-09-09 19:46:39 -0400

    initial commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h2>CONTACT</h2>
+				<p>po†olos pro multis, per. ananeam pro filia à dæmonio vex. ta, per. nturionem pro filio, per batisƒsimam. rginem pro conuiuantibus in. ana. alli –le. erat enim tune temporis ƒupremus no†er med. tor interceƒ. nis, æ vnicus re.</p>
+			</div>
+		);
+	}
+}
+
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<h2>HOME</h2>
+				<p>Quod ƒi h. uƒcemodi ƒeleiorib. quantumuis ling. vfuum recepi ƒacriƒicandi. cleƒia, confuƒa hæc varietas riƒum potius quám d. otionem exteris & peregrines dediet. Quot enim ƒunt in vna ƒola. Ilia ƒermonum altera. o – nes ? quot ƒunt ques in. cem nequeanti. elligi ? idem pro – ponitur de. ƒpanie ,. alie & vtriuƒque. rmaniæ , tum. glie reg. nibus. E† enim vnuic. que innatus par. cula- ris id. matis nati. décor, ad quem profer. dum , pro- nunciat. non omnibus fauet. Sing. læ pariter nat. nes in varias vit. ru, ƒpecies interloq. ndum di†raæ ƒunt, quo minus ab aliis qui vernaculi rat. nem non habent intelligantur. Quidam a. eant. vt bleƒa lingua bland. res videantur, alij ƒpecus latebris. aut fortaƒsis po†remas mbiunt , multi lab. ƒmo abundant. In non paucis eant. damnamus quum vel deduis. ucibus & veulte ë caui ƒpec. latebris æqué receu oris refonat Quid. †ridulos habét pron. ciatonis hiatus , ƒunt qui di. num feces manducant , qu. am captia contondunt, multi po†remas omnes ƒ. labas accuunt. A† fermo facer omn. us cum adipiƒci cup. ntibus ƒeƒe purus, terƒus, or- duis plag. lingu. atinè. thoritas ƒacris mini†er. ve – luti omn. m famigeratiƒsima, & amp. ƒsime prop. ginis fa mul. etur.</p>
+				<p>Quapropt. ƒi memine ris corum quæ ƒuper. ribus paginis dix us , hæc fœdè vituperari ab hæretic. comperies , no†. qu ƒacrific. in†ru auƒpi- cia. udabis , ƒanorúmque interceƒs. nem non minore precio haberi apud. ri†u quâm quæ eide. lluris inco- læ faa e†. atio per. po†olos pro multis, per. ananeam pro filia à dæmonio vex. ta, per. nturionem pro filio, per batisƒsimam. rginem pro conuiuantibus in. ana. alli –le. erat enim tune temporis ƒupremus no†er med. tor interceƒ. nis, æ vnicus re.</p>
+			</div>
+		);
+	}
+}
+
+export default Home;
